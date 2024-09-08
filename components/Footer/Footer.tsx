558999d612ce3e245b9552766a0c6176d995ed07
him18-y <hirayama1008yuta@icloud.com>
2024-09-08 18:17:39 +0900

    型作りました

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <div className='footer'>
+      <div className="p-4 bg-gray-800 text-white">フッター</div>
+    </div>
+  );
+};
+
+export default Footer;
