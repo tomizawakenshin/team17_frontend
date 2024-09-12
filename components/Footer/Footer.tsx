ac1d2c1aa96f3d0501dd3f36ea5dcb9ada0cd0ec
kusunokinami <tosiki2001.39@gmail.com>
2024-09-12 15:52:51 +0900

    フッターのコンポーネントファイルを編集

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react"
+import { FooterMenus } from './FooterMenus'; // FooterMenusファイルからフッターメニューの情報を取得する
+
+import './Footer.css'
 
 const Footer: React.FC = () => {
-  return (
-    <footer className='p-4 bg-gray-800 absolute bottom-0 w-screen'>
-      <div className="text-white">フッター</div>
-    </footer>
-  );
+    return (
+        <footer className='sticky bottom-0 z-10 bg-gray-800'> 
+               <ul className="grid grid-cols-3">
+                {FooterMenus.map((menu, index) => (
+                    <li key={index}>
+                        <a href="{menu.link}" className="flex flex-col items-center justify-center gap-1 py-1.5 text-xs">
+                        {/* フッターメニューの情報を表示する */}
+                        <menu.icon className="iconImqge" stroke="white" size={24}/>
+                        {/* フッターメニューのラベルを表示する(非表示) */}
+                            {/* {menu.label} */}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </footer>
+    );
 };
 
 export default Footer;
