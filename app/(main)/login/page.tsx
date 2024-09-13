39e6425d97293cc17d03c578a2f718ec451f0c97
tomizawakenshin <kenshin.8799@gmail.com>
2024-09-13 19:44:53 +0900

    Loginの処理を行い、認証を行いトークンを発行しなければLogin画面に強制的に遷移するという処理にしました

---
+++ app/(main)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/LoginForm/LoginForm";
+
+const LoginPage = () => {
+    return (
+        <div className='flex flex-col justify-center py-20'>
+            <LoginForm />
+        </div>
+    )
+}
+
+
+export default LoginPage;
