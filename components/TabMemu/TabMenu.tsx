c6ef3934fa17d683c78c00d5d17cdbc3425b7acb
kusunokinami <tosiki2001.39@gmail.com>
2024-09-13 14:30:49 +0900

    日付選択ボタンを作成

---
+++ components/TabMemu/TabMenu.tsx
@@ -0,0 +1,17 @@
+const TabMenu = () => {
+    return (
+        <div className="flex justify-between w-11/12 mx-auto p-2 mt-4 gap-x-2">
+            <button className="border-2 w-full h-full rounded-3xl">
+                9/1
+            </button>
+            <button className="border-2 w-full h-full rounded-3xl">
+                9/2
+            </button>
+            <button className="border-2 w-full h-full rounded-3xl">
+                9/3
+            </button>
+        </div>
+    );
+}
+
+export default TabMenu;
