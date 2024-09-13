5052795b808b9b3c26e1478bcda4fc07b8f42909
tomizawakenshin <kenshin.8799@gmail.com>
2024-09-13 21:14:15 +0900

    不要なコードを削除しました。

--- actions/hanabi.ts
+++ actions/hanabi.ts
@@ -1,10 +1,7 @@
-import { Comment } from "@/models/comment";
 import { Hanabi } from "@/models/hanabi";
-import { Like } from "@/models/like";
-import { User } from "@/models/user";
 
 export const getAllHanabi = async (): Promise<Hanabi[]> => {
-    const res = await fetch(`http://localhost:3002/hanabis`,{
+    const res = await fetch(`http://localhost:3002/hanabis`, {
         cache: "no-store", //SSR
     });
     const hanabis = res.json();
