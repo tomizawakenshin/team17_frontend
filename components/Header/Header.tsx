f34da6939d87cc8140215437a993b17b5654d7ac
kusunokinami <tosiki2001.39@gmail.com>
2024-09-15 04:15:01 +0900

    ヘッダーのサービス名変更

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,9 +6,9 @@ import { RefreshCcw } from 'lucide-react';
 
 const Header = () => {
   return (
-    <header className="h-14 bg-black text-white flex items-center space-x-2 px-2 w-full shadow-md s justify-between p-10 fixed" >
+    <header className="h-14 bg-black text-white flex items-center space-x-2 px-4 w-full shadow-md s justify-between p-10 fixed" >
       <h1 className="p-2 font-bold text-2xl">
-        <Link href="/">Hanabi</Link>
+        <Link href="/">Hidane</Link>
       </h1>
       <div className="p-2 bg-gray-800 rounded-xl">
         <Link href="/home2">
