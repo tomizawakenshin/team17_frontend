a96e36e94d23619f5234ee7bff2597cbea1d1883
kusunokinami <tosiki2001.39@gmail.com>
2024-09-15 04:30:11 +0900

    ヘッダーのアイコンを編集

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className="h-14 bg-black text-white flex items-center space-x-2 px-4 w-full shadow-md s justify-between p-10 fixed" >
       <h1 className="p-2 font-bold text-2xl">
-        <Link href="/">Hidane</Link>
+        <Link href="/home1">Hidane</Link>
       </h1>
       <div className="p-2 bg-gray-800 rounded-xl">
         <Link href="/home2">
