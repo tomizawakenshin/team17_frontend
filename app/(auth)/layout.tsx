0667c00ee024c96a94dd042de9cba3fb62ebdb8a
kusunokinami <tosiki2001.39@gmail.com>
2024-09-15 02:50:18 +0900

    ログイン画面のレイアウト修正

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+const AuthLayout = ({ children }: Readonly<{ children: React.ReactNode; }>) => {
+    return (
+        <div className="flex flex-col h-screen">
+            <main className="flex-grow bg-black text-white overflow-auto">
+                {children}
+            </main>
+        </div>
+    );
+}
+
+export default AuthLayout;
