8d43f896c0892deb572938147f3aa5c204da6f89
Kusu <92491771+kusunokinami@users.noreply.github.com>
2024-09-14 13:12:22 +0900

    Revert "Feature/tabmenu"

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,8 @@
 import Firework from "@/components/Firework/Firework";
-import TabMenu from "@/components/TabMemu/TabMenu";
 
 export default function Home() {
   return (
     <div className="">
-      <TabMenu />
       <Firework />
     </div>
   );
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,29 +1,24 @@
 import React from "react"
 import { FooterMenus } from './FooterMenus'; // FooterMenusファイルからフッターメニューの情報を取得する
 
-
-import Link from 'next/link'
 import './Footer.css'
 
 const Footer: React.FC = () => {
     return (
-
-        <footer className='fixed bottom-0 z-50 bg-gray-800 width-[75%]'> 
+        <footer className='sticky bottom-0 z-10 bg-gray-800'> 
                <ul className="grid grid-cols-3">
                 {FooterMenus.map((menu, index) => (
                     <li key={index}>
-                        <Link href={menu.link} className="flex flex-col items-center justify-center gap-1 py-1.5 text-xs">
+                        <a href="{menu.link}" className="flex flex-col items-center justify-center gap-1 py-1.5 text-xs">
                         {/* フッターメニューの情報を表示する */}
                         <menu.icon className="iconImqge" stroke="white" size={24}/>
                         {/* フッターメニューのラベルを表示する(非表示) */}
                             {/* {menu.label} */}
-                        </Link>
+                        </a>
                     </li>
                 ))}
             </ul>
         </footer>
-
-        
     );
 };
 
