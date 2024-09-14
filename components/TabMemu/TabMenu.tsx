aaca3cb21da31c3b5400c767e71cec951122689a
kusunokinami <tosiki2001.39@gmail.com>
2024-09-14 11:54:30 +0900

    日付表示部分を作成しました

--- components/TabMemu/TabMenu.tsx
+++ components/TabMemu/TabMenu.tsx
@@ -1,16 +1,37 @@
+"use client"
+import { useState } from "react";
+
 const TabMenu = () => {
+
+    // タイトル内の文字を配列で定義
+    const titles = ['9/1', '9/2', '9/3'];
+
+    //タブのクリック状態をuseStateで管理
+    const [clickedTab, setclickedTab] = useState<number | null>(null);
+
+    // クリックイベントハンドラ
+    const handleClick = (index: number) => {
+        setclickedTab(index);
+    }
+
+
     return (
-        <div className="flex justify-between w-11/12 mx-auto p-2 mt-4 gap-x-2">
-            <button className="border-2 w-full h-full rounded-3xl">
-                9/1
+        <div className="flex justify-between w-11/12 mx-auto p-2 mt-4 gap-x-3">
+            {titles.map((title, index) => (
+            <button 
+                key={index}
+                className="border-2 w-full h-full rounded-3xl py-1"
+                onClick={() => handleClick(index)}
+                style={{
+                    backgroundColor: clickedTab === index ? 'white' : 'black',
+                    color: clickedTab === index ? 'black' : 'white',
+                    fontWeight: clickedTab === index ? 'bold' : 'normal',
+                }}
+            >
+            {title}
             </button>
-            <button className="border-2 w-full h-full rounded-3xl">
-                9/2
-            </button>
-            <button className="border-2 w-full h-full rounded-3xl">
-                9/3
-            </button>
-        </div>
+             ))}
+            </div> 
     );
 }
 
