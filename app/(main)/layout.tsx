25e20e978ab42f85adf1df19d100922b6e5115ed
tomizawakenshin <kenshin.8799@gmail.com>
2024-09-15 02:55:45 +0900

    日付タブからその日付の花火を取得できるように改良しました

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -9,10 +9,10 @@ const MainLayout = ({ children }: Readonly<{ children: React.ReactNode; }>) => {
 
     <div className="flex flex-col min-h-screen">
       <Header />
-      
+
       {/* <div className="p-4 bg-gray-800 text-white fixed w-full top-0 left-0 z-50">ヘッダー</div> */}
       <main className="flex-grow mt-12 bg-black text-white">
-      <TabMenu />
+        {/* <TabMenu /> */}
         {children}
       </main>
       <Footer />
